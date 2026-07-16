3009b73e347365db480f0b1bf11001f63808dbbf
chrissmwanja-stack <chriss.mwanja@gmail.com>
2026-07-16 14:47:32 +0300

    changed version from 1.1 to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.example.smetracker"
         minSdk = 24
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 1
+        versionName = "1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
