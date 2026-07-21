bc97db49a8bf2203450bc2afe466f31150ecc26a
chrissmwanja-stack <chriss.mwanja@gmail.com>
2026-07-21 12:02:38 +0300

    updated version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 37
         versionCode = 2
-        versionName = "2.3"
+        versionName = "2.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
